9dc9c25bbfe1c96cbbcd6feee91870df89a4ca27
phentnil <jaxstarling85@gmail.com>
2020-03-07 23:17:07 -0500

    migrating working algorithm from js file to python file

--- htmlcss/kc.js
+++ htmlcss/kc.js
@@ -1,8 +1,9 @@
 /*jshint esversion: 6*/
-/* Unit | Qty | Multiples */
-const getMatches = (target=500,units=[]) => {
-  if (units.length < 1) {return null;}
+function getMatches (target=3000, inUnits=[]) {
+  if (inUnits.length < 1) {return null;}
+  let units = JSON.parse(JSON.stringify(inUnits));
   var matches = [];
+  var results = [];
   while (units.length) {
     let item = units.shift();
     let u = item.unit;
@@ -10,21 +11,31 @@ const getMatches = (target=500,units=[]) => {
     for (let i = 0, l = m.length; i < l; i++) {
       let v = m[i];
       let d = v - target;
+      let combo = String(u) + "x" + String(i);
       if (Math.abs(d) < Math.floor(target * 0.1)) {matches.push({unit: u, quantity: i, value: v, diff: d});}
     }
   }
   matches.sort(sortByDiff);
+  let matchesOut = "Unit | Qty | Value | Difference\n";
   for (let i = 0, j = matches.length; i < j; i++) {
-
+    if (matches[i].unit < 1000) {matchesOut += " ";}
+    matchesOut += matches[i].unit;
+    matchesOut += " |  ";
+    if (matches[i].quantity < 10) {matchesOut += " ";}
+    matchesOut += matches[i].quantity;
+    matchesOut += " |  ";
+    if (matches[i].value < 1000) {matchesOut += " ";}
+    matchesOut += matches[i].value;
+    matchesOut += " | ";
+    if (matches[i].diff >= 0) {matchesOut += "+";}
+    matchesOut += matches[i].diff;
+    if ((i + 1) < j) {
+      matchesOut += "\n";
+    }
   }
-  console.log(matches);
-};
-
-const sortByDiff = (a, b) => Math.abs(a.diff) - Math.abs(b.diff);
-
-const sortByQuantity = (a, b) => a.quantity - b.quantity;
-
-const printUnits = (units=[]) => {
+  console.log(matchesOut);
+}
+function printUnits (units=[]) {
   if (units.length < 1) {return false;}
   let unitOut = "Unit | Qty | Multiples\n";
   for (let i = 0, l = units.length; i < l; i++) {
@@ -42,16 +53,18 @@ const printUnits = (units=[]) => {
     if ((i + 1) < units.length) {unitOut += "\n";}
   }
   console.log(unitOut);
-};
-
-window.onload = () => {
+}
+function sortByDiff (a, b) {return Math.abs(a.diff) - Math.abs(b.diff);}
+function sortByQuantity (a, b) {return a.quantity - b.quantity;}
+function main (target) {
+  if ("number" !== typeof target) {target = 3000;}
   const kcUnits = [{unit: 533, quantity: 4}, {unit: 535, quantity: 3}, {unit: 536, quantity: 1}, {unit: 540, quantity: 6}, {unit: 554, quantity: 1}, {unit: 565, quantity: 2}, {unit: 576, quantity: 2}, {unit: 1097, quantity: 4}, {unit: 1100, quantity: 4}, {unit: 1155, quantity: 2}];
   kcUnits.forEach((item, index, array) => {
     item.multiples = [];
     for (let i = 0; i <= item.quantity; i++) {item.multiples.push(i * item.unit);}
   });
-  const target = 3000;
   console.log("Target:",target);
   printUnits(JSON.parse(JSON.stringify(kcUnits)));
   getMatches(target, JSON.parse(JSON.stringify(kcUnits)));
-};
+}
+window.onload = main;

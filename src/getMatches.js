b51a72a820987cb7a49bd8e8374def07affefd75
phentnil <jaxstarling85@gmail.com>
2020-05-25 23:36:24 -0400

    rework
    Implemented Node.js and made changes to focus on javascript development only. Working toward modularization.

---
+++ src/getMatches.js
@@ -0,0 +1,7 @@
+// TODO: import module that keeps track of units available?
+// Possibly import multiples with individual scores?s
+//const inventory = require("inventory");
+module.exports =  function (target) {
+  if ("undefined" === typeof target) {return {};}
+  var units = [{unit: 533, quantity: 4}, {unit: 535, quantity: 3}, {unit: 536, quantity: 1}, {unit: 1097, quantity: 4}, {unit: 1100, quantity: 4}, {unit: 1155, quantity: 2}];
+};

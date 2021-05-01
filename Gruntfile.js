d08f0802f63ae74a31c5daa70c9259c573639b86
phentnil <jaxstarling85@gmail.com>
2021-04-30 21:51:52 -0400

    Reconfiguring grunt; removing jest and jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,34 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   // Project configuration
   grunt.initConfig({
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'tests/**/*.js'],
+    concat: {
       options: {
-        jshintrc: true
-      }
+        separator: ";\n",
+      },
+      dist: {
+        src: ["./src/result.js", "./src/search.js", "./src/units.js"],
+        dest: "./src/built.js",
+      },
     },
-    pkg: grunt.file.readJSON('package.json'),
-    watch: {
-      scripts: {
-        files: ['<%= jshint.files %>'],
-        tasks: ['default'],
-        options: {
-          spawn: false,
-          event: ['all']
-        }
-      }
-    }
+    uglify: {
+      options: { sourceMap: true, sourceMapName: "./dist/index-sourcemap.map" },
+      target: {
+        files: { "./dist/index.min.js": "./src/built.js" },
+      },
+    },
+    pkg: grunt.file.readJSON("package.json"),
+    nodemon: {
+      dev: {
+        script: "index.js",
+      },
+    },
   });
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-nodemon");
 
   // Default task(s).
-  grunt.registerTask('default', ['test']);
-  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask("default", ["nodemon", "concat", "uglify"]);
 };

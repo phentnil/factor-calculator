0933e68ca67be34c604a629aa6d7ff2734980d9f
phentnil <jaxstarling85@gmail.com>
2022-03-13 01:04:13 -0500

    Consolidate output to index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 const { search } = require("./search");
-search(3300);
+const target = 3300;
+const results = search(3300);
+
+console.log(`Target: ${target}`);
+console.log(
+  `${"Sum".padStart(4, " ").padEnd(6, " ")}${"Diff"
+    .padStart(5, " ")
+    .padEnd(8, " ")}Units`
+);
+console.log(results.map((mult) => mult.simplePrint()).join("\n"));

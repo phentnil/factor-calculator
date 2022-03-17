79b1e5989ecdac16cfa872b7e1c901d6a0517a67
phentnil <jaxstarling85@gmail.com>
2022-03-17 11:46:57 -0400

    Updated for HTML output

--- src/index.js
+++ src/index.js
@@ -1,11 +1,77 @@
 const { search } = require("./search");
-const target = 3300;
-const results = search(3300);
+const factorTable = document.getElementById("factorResults");
+const targetInput = document.getElementById("newTargetInput");
+const targetHead = document.getElementById("targetValue");
+const resultsInRange = document.getElementById("resultsInRange");
+const tableRow = document.createElement("tr");
+const tableCell = document.createElement("td");
 
-console.log(`Target: ${target}`);
-console.log(
-  `${"Sum".padStart(4, " ").padEnd(6, " ")}${"Diff"
-    .padStart(5, " ")
-    .padEnd(8, " ")}Units`
-);
-console.log(results.map((mult) => mult.simplePrint()).join("\n"));
+var allowedKeys = [
+  "Backspace",
+  "Alt",
+  "Control",
+  "Shift",
+  "End",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowUp",
+  "ArrowDown",
+  "Delete",
+  "Home",
+  "PageUp",
+  "PageDown",
+  "Insert",
+  "Tab",
+  "Escape",
+];
+var results;
+var target = targetInput.value;
+console.log(target, typeof target, target.length);
+targetInput.addEventListener("keydown", (event) => {
+  if (!allowedKeys.includes(event.key) && event.key.match(/\d+/gi) === null) {
+    event.preventDefault();
+    console.log(event);
+    console.log(event.key);
+  }
+});
+const reloadResults = () => {
+  target = parseInt(targetInput.value, 10);
+  results = search(target);
+  targetHead.textContent = target;
+  factorTable.innerHTML = "";
+  results = results.filter((res) => res.differencePercent < 0.1);
+  const inRangeResults = results.filter((res) => res.differencePercent > -0.1);
+  resultsInRange.textContent = inRangeResults.length;
+  results.forEach((result) => {
+    const row = tableRow.cloneNode();
+    const sumCell = tableCell.cloneNode();
+    const diffCell = tableCell.cloneNode();
+    const unitsCell = tableCell.cloneNode();
+
+    sumCell.textContent = result.sum;
+    diffCell.textContent = `${result.difference} (${(
+      result.differencePercent * 100
+    ).toPrecision(3)}%)`;
+    unitsCell.textContent = result.units.join(", ");
+    row.appendChild(sumCell);
+    row.appendChild(diffCell);
+    row.appendChild(unitsCell);
+    factorTable.appendChild(row);
+  });
+};
+targetInput.addEventListener("keyup", (event) => {
+  if (event.target.value < 400 || event.target.value > 5000) {
+    event.preventDefault();
+    return false;
+  }
+  reloadResults();
+});
+targetInput.addEventListener("change", (event) => {
+  if (event.target.value < 400 || event.target.value > 5000) {
+    event.preventDefault();
+    return false;
+  }
+  reloadResults();
+});
+
+reloadResults();

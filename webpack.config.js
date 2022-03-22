9308ef5c67afe034884099b8c5ac435e3cdc8baf
Jason Starling <25496955+phentnil@users.noreply.github.com>
2022-03-21 20:26:28 -0400

    Update webpack.config.js
    Updating for production

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   module: {
     rules: [
       {

042039cb0b4575b81a6f62cbede860f6ab878c25
phentnil <jaxstarling85@gmail.com>
2022-04-05 16:09:06 -0400

    Introduced getters and setters

--- src/Unit.js
+++ src/Unit.js
@@ -1,17 +1,32 @@
 class Unit {
-  value;
-  quantity;
-  multiples;
+  _unit;
+  _quantity;
+  _multiples;
   constructor(unit, quantity) {
-    this.value = unit;
-    this.quantity = quantity;
-    let multiples = [];
-    let amount = unit;
-    for (let i = 1; i <= quantity && amount <= 5000; i++) {
-      multiples.push(amount);
-      amount += unit;
+    this._unit = unit;
+    this._quantity = quantity;
+    for (let i = 1, sum = unit; i <= quantity && sum <= MAX_UNIT_VALUE; i++) {
+      this._multiples.push(sum);
+      sum += unit;
     }
-    this.multiples = multiples;
+  }
+  get unit() {
+    return this._unit;
+  }
+  set unit(unit) {
+    this._unit = unit;
+  }
+  get quantity() {
+    return this._quantity;
+  }
+  set quantity(quantity) {
+    this._quantity = quantity;
+  }
+  get multiples() {
+    return this._multiples;
+  }
+  set multiples(multiples) {
+    this._multiples = multiples;
   }
 }
 module.exports = { Unit };
